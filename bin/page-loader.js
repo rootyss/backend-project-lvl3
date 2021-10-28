40d6b9a05e353949215d6552263ef4bbd3508726
rootyss <jaxent@yandex.ru>
2021-10-28 18:26:18 +0300

    delete console.log

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log('kek');
+import Command from 'commander';
+import pageLoader from '../src/pageLoader.js';
+
+const pageLoaderCLI = Command;
+
+pageLoaderCLI
+  .version('1.0.0')
+  .description('Page loader utility')
+  .arguments('<url>')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .action((url) => {
+    pageLoader(url, pageLoaderCLI.opts().output);
+  });
+
+pageLoaderCLI.parse(process.argv);
