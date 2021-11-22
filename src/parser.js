359eb552f9d8eb3296db702743003581607bac41
rootyss <jaxent@yandex.ru>
2021-11-22 16:18:08 +0300

    add parser page

---
+++ src/parser.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import url from 'url';
+import cheerio from 'cheerio';
+
+const tagsMapping = {
+  link: 'href',
+  img: 'src',
+  script: 'src',
+};
+
+const parse = (page) => {
+  const links = [];
+  const $ = cheerio.load(page);
+  _.keys(tagsMapping).forEach((el) => {
+    $(el).each((i, e) => {
+      const a = $(e).attr(tagsMapping[el]);
+      if (a) {
+        const currentA = a.slice(0, a.includes('?') ? a.indexOf('?') : a.length);
+        links.push(currentA);
+      }
+    });
+  });
+  const relativeLinks = links.filter((i) => {
+    const { host } = url.parse(i);
+    return !host;
+  });
+  return relativeLinks;
+};
+
+export default parse;
