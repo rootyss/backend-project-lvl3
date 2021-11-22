306bf17048000e6eed5db971c2839ea549e1bac5
rootyss <jaxent@yandex.ru>
2021-11-22 16:21:44 +0300

    add getNameFromLink

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,23 @@
-import axios from 'axios';
 import url from 'url';
-
-export const getStream = (link) => axios.get(link);
+import path from 'path';
 
 export const getKebabCasedLink = (link) => {
   const { host, path: linkPath } = url.parse(link);
   const name = `${host || ''}${linkPath}`.replace(/[^a-z1-9]/g, '-');
   return name.split('-').filter((i) => i).join('-');
+};
+
+export const getNameFromLink = (link, type = 'file') => {
+  const urlInKebabCase = getKebabCasedLink(link);
+
+  switch (type) {
+    case 'file': {
+      const ext = path.extname(link) || '.html';
+      return `${urlInKebabCase}${ext}`;
+    }
+    case 'directory': return `${urlInKebabCase}_files`;
+    default: return 'none';
+  }
 };
 
 export const getHtmlFileName = (link) => {
