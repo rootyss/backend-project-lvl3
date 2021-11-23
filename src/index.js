ca3c6d6279bb0a5bd5749ba429173d40168199c0
rootyss <jaxent@yandex.ru>
2021-11-23 19:12:54 +0300

    fix problem with dl

--- src/index.js
+++ src/index.js
@@ -82,17 +82,6 @@ export const loadResources = (loadedUrl, outputPath, page, hostname) => {
 };
 
 export default (loadedUrl, outputPath = '') => {
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  console.log('Путь стал: ', outputPath);
-  
   const { host: hostname } = new URL(loadedUrl);
   const sourceDir = getNameFromLink(loadedUrl, 'directory');
   return axios.get(loadedUrl)
